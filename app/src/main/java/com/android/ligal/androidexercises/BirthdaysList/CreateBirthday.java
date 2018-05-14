1b5476ecd5ba13d430bbafe3385359f1c3b8be54
LigalLevy <37476027+LigalLevy@users.noreply.github.com>
2018-05-14 23:53:21 +0300

    fixed date picker

--- app/src/main/java/com/android/ligal/androidexercises/BirthdaysList/CreateBirthday.java
+++ app/src/main/java/com/android/ligal/androidexercises/BirthdaysList/CreateBirthday.java
@@ -1,5 +1,6 @@
 package com.android.ligal.androidexercises.BirthdaysList;
 
+import android.app.DatePickerDialog;
 import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,18 +8,22 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.android.ligal.androidexercises.R;
+
+import java.util.Calendar;
 
 public class CreateBirthday extends AppCompatActivity {
 
     EditText name;
     EditText birthDate;
-    CalendarView calendarView;
     EditText comment;
     Button button;
+    String date;
+    int day, month, year;
+    Calendar mCurrentDate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,38 +32,45 @@ public class CreateBirthday extends AppCompatActivity {
 
         name = findViewById(R.id.name);
         birthDate = findViewById(R.id.birth_date);
-        calendarView = findViewById(R.id.calendar_view);
         comment = findViewById(R.id.comment);
         button = findViewById(R.id.buttonAdd);
 
-        final BirthdaysListDatabase db = Room.databaseBuilder(getApplicationContext(), BirthdaysListDatabase.class, "BirthDays List")
+        mCurrentDate = Calendar.getInstance();
+
+        day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+        month = mCurrentDate.get(Calendar.MONTH);
+        year = mCurrentDate.get(Calendar.YEAR);
+        month = month+1;
+
+
+        final BirthdaysListDatabase db = Room.databaseBuilder(getApplicationContext(), BirthdaysListDatabase.class, "BirthdaysList")
                 .allowMainThreadQueries()
                 .build();
 
         birthDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calendarView.setVisibility(View.VISIBLE);
-            }
-        });
-
-        calendarView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                long date = calendarView.getDate();
-                birthDate.setText(String.valueOf(date));
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateBirthday.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month = month+1;
+                        birthDate.setText(dayOfMonth+"/"+month+"/"+year);
+                        date = dayOfMonth+"/"+month+"/"+year;
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
             }
         });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: make a database
-                db.birthDaysListDao().insertAll(new BirthDayList(name.getText().toString(),"",comment.getText().toString()));
+                db.birthDaysListDao().insertAll(new BirthDayList(
+                        name.getText().toString(),
+                        birthDate.getText().toString(),
+                        comment.getText().toString()));
                 startActivity(new Intent(CreateBirthday.this, BirthdayActivity.class));
-
             }
         });
-
     }
 }
